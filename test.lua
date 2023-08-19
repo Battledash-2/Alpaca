a36f78d8a0f93f893ca3fc782d7a995fbba1d61f
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2023-08-19 17:50:14 -0400

    fix this

--- test.lua
+++ test.lua
@@ -2,10 +2,6 @@ local DEFAULT_NAME = "User";
 
 local name = io.read("What's your name?");
 
-print(name)
-print(nil)
-print(name == "" or name == nil)
-
 if name == "" or name == nil then
 	print("No name provided, defaulting to " .. DEFAULT_NAME .. ".");
 	name = DEFAULT_NAME;
